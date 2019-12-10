ddb5e820d659d58ab2fd5acb31bef4249bcc6ec6
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-12-10 17:52:29 +0200

    add (image-section.vue): base markup and style

---
+++ src/components/image-section.vue
@@ -0,0 +1,14 @@
+<template>
+   <section class="grid-section image-section" :style="{background: `url(${src})`}">
+   </section>
+</template>
+
+<script>
+export default{
+    name: "image-section",
+    props: ['src']
+}
+</script>
+
+<style scope>
+</style>
