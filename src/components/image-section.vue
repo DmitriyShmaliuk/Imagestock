e8169710aa45c474c0e5673b1c6c161cba1b96b8
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-12-11 11:10:13 +0200

    update (image-section.vue): markup

--- src/components/image-section.vue
+++ src/components/image-section.vue
@@ -1,12 +1,17 @@
 <template>
-   <section class="grid-section image-section" :style="{background: `url(${src})`}">
+   <section class="grid-section image-section" :style="{'background-image': `url(${src})`}">
    </section>
 </template>
 
 <script>
 export default{
     name: "image-section",
-    props: ['src']
+    props: {
+       src: {
+          type: String,
+          required: true
+       }
+    }
 }
 </script>
 
