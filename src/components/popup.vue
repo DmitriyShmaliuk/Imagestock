db6abf11e1c30091f18623a45e7379820cb91fc7
d.shmaliuk <d.shmaliuk@viseven.com>
2020-01-23 10:36:05 +0200

    add (popup.vue): image section

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -2,29 +2,49 @@
     <v-container class="background" v-if="isPopupOpened">
         <v-container class="popup">
             <v-container class="close-icon"
-                         @click="$emit('update:isPopupOpened')">
+                         @click="$emit('update:isPopupOpened', false)">
                 <v-icon color="#21b8c6"
                         :size="35"
                 >mdi-close</v-icon>
             </v-container>
 
-            <v-container class="image-section">
+            <v-container class="popup-content">
+                <v-container class="image-section">
+                    <v-img :src="src"
+                            :height="550"
+                            width="auto"
+                            alt="no-image"></v-img>
+                </v-container>
 
+                <v-container class="reaction-section">
+                    <v-container class="dislike-icon">
+
+                    </v-container>
+                    <v-container class="like-icon">
+
+                    </v-container>
+                </v-container>
             </v-container>
         </v-container>
     </v-container>
 </template>
 
 <script>
-    export default{
-        name: "popup",
-        props: {
-            isPopupOpened: {
-                type: Boolean,
-                default: false
-            }
+import defaultImage from '../assets/no-image.png';
+
+export default{
+    name: "popup",
+    props: {
+        isPopupOpened: {
+            type: Boolean,
+            default: false
+        },
+        src: {
+            type: String,
+            default: defaultImage,
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -52,17 +72,37 @@
                 top: 0;
                 right: 0;
                 width: auto;
-
             }
 
-            .image-section {
+            .popup-content{
                 position: absolute;
                 top: 0;
                 left: 0;
                 width: 450px;
-                height: 515px;
-                background-color: #f3f4f2;
+                padding: 0;
+
+                .image-section {
+                    width: 100%;
+                    height: 515px;
+                    padding: 0;
+                    overflow: hidden;
+                }
+
+                .reaction-section{
+                    position: relative;
+                    width: 100%;
+                    height: 60px;
+                    background-color: #f5f6f4;
+
+                    .dislike-icon, .like-icon{
+                        width: 65px;
+                        height: 60px;
+
+                        border: 1px solid red;
+                    }
+                }
             }
+
         }
     }
 </style>
