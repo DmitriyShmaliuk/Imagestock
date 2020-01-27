31d593d27d9a6b12a4591996ccfa58f5c74aaf47
Dmytro Shmaliuk <dimkas2001@icloud.com>
2020-01-27 10:47:09 +0200

    add(all): Vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import Vuex from 'vuex';
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+
