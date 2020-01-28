6415315c5e2089d7c46bee3e9793b3075b9ffdaa
Dmytro Shmaliuk <dimkas2001@icloud.com>
2020-01-28 22:35:08 +0200

    add (store.js): images in state, add mutation and add action

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,17 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state:{
-
+    state: {
+        images: []
+    },
+    mutations: {
+        add(state, element){
+            state.images.push(element);
+        }
+    },
+    actions: {
+        add({commit}, element){
+            commit('add', element);
+        }
     }
 });
