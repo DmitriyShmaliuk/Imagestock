98e101e49fb07850a880f1fc0e90c75c5cca5582
Dmytro Shmaliuk <dimkas2001@icloud.com>
2020-01-29 12:04:29 +0200

    add (store.js): currentIndex properties and clear code

--- src/store/store.js
+++ src/store/store.js
@@ -6,16 +6,23 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         images: [],
+        currentIndex: null,
     },
     mutations: {
-        addImage(state, element){
-            state.images.push(element);
+        addImage({images}, element){
+            images.push(element);
+        },
+        setCurrentIndex(state, index){
+            state.currentIndex = (index >=0 && index < state.images.length)? index: state.currentIndex;
         }
     },
     actions: {
         addImage({state,commit}, element){
             commit('addImage', element);
             localStorage.setItem("images-store", JSON.stringify(state.images));
+        },
+        setCurrentIndex({commit}, index){
+            commit('setCurrentIndex', index);
         }
     }
 });
