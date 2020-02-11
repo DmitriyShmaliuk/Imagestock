144cc7bba564dfa738754d6127d3623970e192ee
Dmytro Shmaliuk <dimkas2001@icloud.com>
2020-02-11 22:03:31 +0200

    update (popup.vue): clear component

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -6,8 +6,6 @@
                              @click="$emit('update:isPopupOpened', false)">
                     <v-icon color="#21b8c6" :size="35">mdi-close</v-icon>
                 </v-container>
-
-                <slot></slot>
             </v-container>
         </v-container>
     </transition>
