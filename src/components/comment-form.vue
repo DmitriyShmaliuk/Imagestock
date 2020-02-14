c1a83dc82c212e1e309834b1fa0a7e774095bbbf
Dmytro Shmaliuk <dimkas2001@icloud.com>
2020-02-14 16:10:36 +0200

    update (comment-form.vue): function 'sendInformation'

--- src/components/comment-form.vue
+++ src/components/comment-form.vue
@@ -23,9 +23,10 @@ export default{
     },
     methods:{
         sendInformation(){
-            this.$emit('send-info', {userName: this.userName, userComment: this.userComment});
-            this.userName = "";
-            this.userComment = "";
+            if (this.userName.length >= 2 && this.userComment.length >= 3){
+                this.$emit('send-info', {userName: this.userName, userComment: this.userComment});
+                this.userName = this.userComment = "";
+            }
         }
     }
 }
@@ -46,6 +47,7 @@ export default{
             margin-top: 11px;
             width: 225px;
             height: 50px;
+            resize: none;
             border-radius: 3px;
             border: 1px solid #a0b0ba;
 
